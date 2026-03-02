c2eabfc782db9bcb2a5c02972f029796bda155f4
Vinzette <apoorva.kr03@gmail.com>
2026-03-02 13:15:41 +0000

    RAM based persistence

--- chatbot.py
+++ chatbot.py
@@ -3,6 +3,7 @@ from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_openai import ChatOpenAI
 from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import MemorySaver
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -22,16 +23,27 @@ def chat_node(state: ChatState):
     return {'messages': [response]}
 
 
+checkpointer = MemorySaver()
 graph = StateGraph(ChatState)
 #adding nodes
 graph.add_node('chat_node', chat_node)
 #eges
 graph.add_edge(START, 'chat_node')
 graph.add_edge('chat_node', END)
-chatbot = graph.compile()
+chatbot = graph.compile(checkpointer=checkpointer)
 
 initial_state={
     'messages': [HumanMessage(content="What is the capital of Japan")]
 }
 
-print(chatbot.invoke(initial_state))
+#(chatbot.invoke(initial_state)['messages'][-1].content)
+thread_id = 1
+while True:
+    user_message = input("Type here: ")
+    print('User:', user_message)
+
+    if user_message.strip().lower() in ['quit', 'exit', 'bye']:
+        break
+    config = {'configurable':{"thread_id" : thread_id}}
+    response = chatbot.invoke({'messages': [HumanMessage(content=user_message)]}, config=config)
+    print('AI:', response['messages'][-1].content)
