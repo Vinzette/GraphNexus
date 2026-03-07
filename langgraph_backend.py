c06c784d9c7001598268ebd4d7f0c7306f2c834e
Vinzette <apoorva.kr03@gmail.com>
2026-03-07 15:04:10 +0000

    implement stock price, calc and duckduckgo tool

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -1,4 +1,4 @@
-from langgraph.graph import StateGraph, START, END
+from langgraph.graph import StateGraph, START
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_openai import ChatOpenAI
@@ -6,6 +6,11 @@ from langgraph.graph.message import add_messages
 from langgraph.checkpoint.sqlite import SqliteSaver
 from dotenv import load_dotenv
 import sqlite3
+import os
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_core.tools import tool
+import requests # is used for making HTTP requests (e.g., GET, POST) to web services and APIs.
 
 load_dotenv()
 
@@ -15,16 +20,71 @@ class ChatState(TypedDict):
 
 
 llm = ChatOpenAI()
+ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+
+# Tools
+search_tool = DuckDuckGoSearchRun(region="us-en")
+
+@tool
+def calculator(first_num: float, second_num: float, operation: str) -> dict:
+    """
+    Perform a basic arithmetic operation on two numbers.
+    Supported operations: add, sub, mul, div
+    """
+    try:
+        if operation == "add":
+            result = first_num + second_num
+        elif operation == "sub":
+            result = first_num - second_num
+        elif operation == "mul":
+            result = first_num * second_num
+        elif operation == "div":
+            if second_num == 0:
+                return {"error": "Division by zero is not allowed"}
+            result = first_num / second_num
+        else:
+            return {"error": f"Unsupported operation '{operation}'"}
+        
+        return {"first_num": first_num, "second_num": second_num, "operation": operation, "result": result}
+    except Exception as e:
+        return {"error": str(e)}
+
+
+
+
+@tool
+def get_stock_price(symbol: str) -> dict:
+    """
+    Fetch latest stock price for a given symbol (e.g. 'AAPL', 'TSLA')
+    using Alpha Vantage with API key loaded from the environment.
+    """
+    if not ALPHAVANTAGE_API_KEY:
+        return {"error": "Missing ALPHAVANTAGE_API_KEY in environment variables"}
+
+    url = (
+        "https://www.alphavantage.co/query"
+        f"?function=GLOBAL_QUOTE&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
+    )
+    r = requests.get(url)
+    return r.json()
+
+
+
+tools = [search_tool, get_stock_price, calculator]
+llm_with_tools = llm.bind_tools(tools)
 
 
 # node def
 def chat_node(state: ChatState):
+    """LLM node that may answer or request a tool call."""
     # take user query
     messages = state["messages"]
     # seed to llm
-    response = llm.invoke(messages)
+    response = llm_with_tools.invoke(messages)
     # append back to history
     return {"messages": [response]}
+
+tool_node =  ToolNode(tools)
 
 conn = sqlite3.connect(database='chatbot.db', check_same_thread=False) #same db will be used in diff threads
 checkpointer = SqliteSaver(conn=conn)
@@ -32,9 +92,11 @@ checkpointer = SqliteSaver(conn=conn)
 graph = StateGraph(ChatState)
 # adding nodes
 graph.add_node("chat_node", chat_node)
-# eges
+graph.add_node("tools", tool_node)
+# edges
 graph.add_edge(START, "chat_node")
-graph.add_edge("chat_node", END)
+graph.add_conditional_edges("chat_node", tools_condition)
+graph.add_edge('tools', 'chat_node')
 chatbot = graph.compile(checkpointer=checkpointer)
 
 def retrieve_all_threads():
@@ -43,7 +105,7 @@ def retrieve_all_threads():
         all_threads.add(checkpoint.config['configurable']['thread_id'])
     return list(all_threads)
 
-initial_state = {"messages": [HumanMessage(content="What is the capital of Japan")]}
+# initial_state = {"messages": [HumanMessage(content="What is the capital of Japan")]}
 
 # (chatbot.invoke(initial_state)['messages'][-1].content)
 if __name__ == "__main__":
